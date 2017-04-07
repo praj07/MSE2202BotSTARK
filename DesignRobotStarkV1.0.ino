3e5b6bae305be400ce45fd1c39adc2715c90b1c0
Paavan Raj <paavan.raj@hotmail.com>
2017-04-07 03:11:05 -0400

    Add files via upload

--- DesignRobotStarkV1.0.ino
+++ DesignRobotStarkV1.0.ino
@@ -18,7 +18,9 @@ I2CEncoder encoder_RightMotor;
 const int ci_Ultrasonic_PingB = 2 ; //output plug back US
 const int ci_Ultrasonic_DataB = 3 ; //input plug back US
 const int ci_Ultrasonic_PingF = 4 ; //output plug front US
-const int ci_Ultrasonic_DataF = 5 ; //input plug front Us
+const int ci_Ultrasonic_DataF = 5 ; //input plug front US
+const int ci_Ultrasonic_Ping_Pyra; // output plug for pyramid US
+const int ci_Ultrasonic_Data_Pyra; //input plug for pyramid US
 const int ci_Elevator = 6;
 const int ci_Mode_Switch = 7;
 const int ci_Right_Motor = 8;
@@ -35,14 +37,21 @@ const int ci_I2C_SCL = A5;
 
 unsigned long ul_Echo_TimeF;
 unsigned long ul_Echo_TimeB;
+unsigned long ul_Echo_TimePyramid;
 
 int Hall_Reading;
+int servo_Pos_Tess;
+int servo_Pos_Pyra;
 
 long l_Elevator_Motor_Position;
 long l_Left_Motor_Position;
 long l_Right_Motor_Position;
 
-int state_Var = 1;
+unsigned long ul_Left_Motor_Speed;
+unsigned long ul_Right_Motor_Speed;
+unsigned long ul_Elevator_Motor_Speed;
+
+int state_Var = 0;
 
 bool runtime = false;
 
@@ -85,67 +94,97 @@ void setup() {
   //Control Pin
   pinMode(ci_Mode_Switch, INPUT);
   pinMode(ci_HallEffect, INPUT);
-
+  StarkTesseractClawOpen();
 }
 
 void loop() {
   Hall_Reading = analogRead(ci_HallEffect);
-  Serial.println(encoder_Elevator.getRawPosition());
-  if (encoder_Elevator.getRawPosition() >= -100)
-  {
-    StarkElevatorUp();
-  }
-  if (encoder_Elevator.getRawPosition() <= -100) {
-    StarkTesseractClawOpen();
-    servo_ElevatorMotor.writeMicroseconds(1500);
-  }
-  Serial.println(Hall_Reading);
-  if (state_Var == 1)
-  {
-    if (abs(Hall_Reading - 514) > 4) {
-      servo_TesseractClaw.write(20);
-    }
-  }
+  //servo_TesseractClaw.detach();
+  //servo_PyramidClaw.detach();
+  //servo_ElevatorMotor.detach();
+  Serial.println(Hall_Reading - 514);
 
   //This is the actual RUNNING CODE//
-  if (digitalRead(ci_Mode_Switch) == LOW)
-    runtime = false;
-  else
-    runtime = true;
-  if (runtime) {
-    if (state_Var == 0) { //Drive along right wall
-      PingBack();
-      PingFront();
-      if (ul_Echo_TimeB / 58 == 7 && ul_Echo_TimeF / 58 == 7) {
-        if (ul_Echo_TimeB == ul_Echo_TimeF) {
-          StarkGoStraight();
-        }
-        else if (ul_Echo_TimeB - ul_Echo_TimeF > 0) {
-          StarkTurnRight();
-        }
-        else if (ul_Echo_TimeB - ul_Echo_TimeF < 0) {
-          StarkTurnLeft();
-        }
-      }
-      else if (ul_Echo_TimeB / 58 > 7 && ul_Echo_TimeF / 58 > 7 ) {
-        StarkTurnLeft();
-      }
-      else if (ul_Echo_TimeB / 58 < 7 && ul_Echo_TimeF / 58 < 7) {
-        StarkTurnRight();
-      }
-      if (abs(Hall_Reading - 514) > 4){
+
+  if (state_Var == 0) { //Drive along right wall
+    // PingBack();
+    //PingFront();
+    //if ((ul_Echo_TimeB - ul_Echo_TimeF > 40) && (ul_Echo_TimeB - ul_Echo_TimeF < 90)) {
+    // Serial.println("Go Straight");
+    StarkGoStraight();
+    AntiGrav();
+    if (abs(Hall_Reading - 514) > 15) {
+      state_Var++;
+    }
+  }
+  if (state_Var == 1) { //Moving Elevator into position
+    if (encoder_Elevator.getRawPosition() < 33) {
+      StarkElevatorDown();
+    }
+    if (encoder_Elevator.getRawPosition() >= 33) {
+      AntiGrav();
+      StarkTesseractClawClose();
+      encoder_LeftMotor.zero();
+      state_Var++;
+    }
+  }
+  if (state_Var == 2) {
+    if (encoder_LeftMotor.getRawPosition() < 70) {
+      StarkTurnRight();
+    }
+    else if (encoder_LeftMotor.getRawPosition() > 70 && encoder_LeftMotor.getRawPosition() < 120)
+    {
+      StarkGoStraight();
+    }
+    else if (encoder_LeftMotor.getRawPosition() >= 120)
+    { StarkSweepCW();
+    }
+
+    if (Serial.available() > 0) {
+      if (Serial.read() == 69 || Serial.read() == 65) {
         state_Var++;
       }
     }
-    if (state_Var ==2){//Moving Elevator into position
-      
   }
-  else {
-    servo_RightDriveMotor.writeMicroseconds(1500);
-    servo_LeftDriveMotor.writeMicroseconds(1500);
+
+  if (state_Var == 3) {
+    PingPyramid();
+    StarkGoStraight();
+    if (Serial.available() > 0) {
+      if (Serial.read() != 69 && Serial.read() != 65) {
+        state_Var = 2;
+      }
+    }
+    if (ul_Echo_TimePyramid / 58 == 7) {
+      StarkPyramidClawClose() ;
+      StarkTesseractClawOpen();
+      encoder_RightMotor.zero();
+      encoder_Elevator.zero();
+      state_Var++;
+    }
   }
-}
-//state_Var++;
+  if (state_Var == 4) {
+    if (encoder_Elevator.getRawPosition() < 100) {
+      StarkMotorUp()
+    }
+    if (encoder_Elevator.getRawPosition() >= 100){
+      state_Var++;
+    }
+  }
+if (state_Var == 5) {
+    if (encoder_RightMotor.getRawPosition() < 138) {
+      StarkSweepCCW();
+    }
+    if (encoder_RightMotor.getRawPosition() >= 138) {
+      StarkPyramidClawOpen();
+    }
+  }
+
+  servo_RightDriveMotor.writeMicroseconds(ul_Right_Motor_Speed);
+  servo_LeftDriveMotor.writeMicroseconds(ul_Left_Motor_Speed);
+  servo_ElevatorMotor.writeMicroseconds(ul_Elevator_Motor_Speed);
+  servo_TesseractClaw.write(servo_Pos_Tess);
+  servo_PyramidClaw.write(servo_Pos_Pyra);
 }
 
 void PingFront()
@@ -158,6 +197,18 @@ void PingFront()
   //use command pulseIn to listen to Ultrasonic_Data pin to record the
   //time that it takes from when the Pin goes HIGH until it goes LOW
   ul_Echo_TimeF = pulseIn(ci_Ultrasonic_DataF, HIGH, 10000);
+}
+
+void PingPyramid()
+{
+  //Ping Ultrasonic
+  //Send the Ultrasonic Range Finder a 10 microsecond pulse per tech spec
+  digitalWrite(ci_Ultrasonic_PingPyra, HIGH);
+  delayMicroseconds(10);  //The 10 microsecond pause where the pulse in "high"
+  digitalWrite(ci_Ultrasonic_PingPyra, LOW);
+  //use command pulseIn to listen to Ultrasonic_Data pin to record the
+  //time that it takes from when the Pin goes HIGH until it goes LOW
+  ul_Echo_TimePyramid = pulseIn(ci_Ultrasonic_DataPyramid, HIGH, 10000);
 }
 
 void PingBack()
@@ -180,28 +231,74 @@ void PingBack()
 
 
 void StarkGoStraight() {
-  servo_RightDriveMotor.writeMicroseconds(1900);
-  servo_LeftDriveMotor.writeMicroseconds(1900);
+  ul_Right_Motor_Speed = 1700;
+  ul_Left_Motor_Speed = 1700;
+  ul_Elevator_Motor_Speed = 1450;
 }
 void StarkTurnRight() {
-  servo_RightDriveMotor.writeMicroseconds(1500);
-  servo_LeftDriveMotor.writeMicroseconds(1900);
+  ul_Right_Motor_Speed = 1600;
+  ul_Left_Motor_Speed = 1900;
+  ul_Elevator_Motor_Speed = 1450;
 }
 void StarkTurnLeft() {
-  servo_RightDriveMotor.writeMicroseconds(1900);
-  servo_LeftDriveMotor.writeMicroseconds(1500);
+  ul_Right_Motor_Speed = 1700;
+  ul_Left_Motor_Speed = 1500;
+  ul_Elevator_Motor_Speed = 1450;
 }
 void StarkTesseractClawOpen() {
-  servo_TesseractClaw.write(150);
+  servo_Pos_Tess = 70;
+  ul_Right_Motor_Speed = 1500;
+  ul_Left_Motor_Speed = 1500;
+  ul_Elevator_Motor_Speed = 1450;
 }
 void StarkTesseractClawClose() {
-  servo_TesseractClaw.write(20);
+  servo_Pos_Tess = 10;
+  ul_Right_Motor_Speed = 1500;
+  ul_Left_Motor_Speed = 1500;
+  ul_Elevator_Motor_Speed = 1450;
+}
+void StarkPyramidClawOpen() {
+  servo_Pos_Pyra = 10;
+  ul_Right_Motor_Speed = 1500;
+  ul_Left_Motor_Speed = 1500;
+  ul_Elevator_Motor_Speed = 1450;
+}
+void StarkPyramidClawClose() {
+  servo_Pos_Tess = 70;
+  ul_Right_Motor_Speed = 1500;
+  ul_Left_Motor_Speed = 1500;
+  ul_Elevator_Motor_Speed = 1450;
 }
 void StarkElevatorUp()
 {
-  servo_ElevatorMotor.writeMicroseconds(1700);
+  ul_Elevator_Motor_Speed = 1400;
+  ul_Right_Motor_Speed = 1500;
+  ul_Left_Motor_Speed = 1500;
 }
 void StarkElevatorDown()
 {
-  servo_ElevatorMotor.writeMicroseconds(1400);
+  ul_Elevator_Motor_Speed = 1600;
+  ul_Right_Motor_Speed = 1500;
+  ul_Left_Motor_Speed = 1500;
 }
+void StarkElevatorStop() {
+  ul_Elevator_Motor_Speed = 1500;
+  ul_Right_Motor_Speed = 1500;
+  ul_Left_Motor_Speed = 1500;
+}
+void AntiGrav() {
+  ul_Elevator_Motor_Speed = 1450;
+  // ul_Right_Motor_Speed = 1500;
+  // ul_Left_Motor_Speed = 1500;
+}
+void StarkSweepCW() {
+  ul_Right_Motor_Speed = 1300;
+  ul_Left_Motor_Speed = 1700;
+  ul_Elevator_Motor_Speed = 1450;
+}
+void StarkSweepCCW() {
+  ul_Right_Motor_Speed = 1700;
+  ul_Left_Motor_Speed = 1300;
+  ul_Elevator_Motor_Speed = 1450;
+}
+
